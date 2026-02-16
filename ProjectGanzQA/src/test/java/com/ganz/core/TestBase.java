7d4cfca4721e071f36a9da06c7e08a74ea0254c6
Oleh Hanziienko <ohanziienko@hotmail.com>
2026-02-16 19:54:17 +0100

    refactor: fix project structure to src/test/java and rename package to ui

--- ProjectGanzQA/src/test/java/com/ganz/core/TestBase.java
+++ ProjectGanzQA/src/test/java/com/ganz/core/TestBase.java
@@ -1,44 +1,32 @@
 package com.ganz.core;
 
-import com.ganz.core.ApplicationManager;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.edge.EdgeOptions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
-public class TestBase  {
+public class TestBase {
 
     protected static ApplicationManager app = new ApplicationManager();
-    protected Logger logger = LoggerFactory.getLogger(TestBase.class);
+    protected static Logger logger = LoggerFactory.getLogger(TestBase.class);
 
     @BeforeMethod
     public void setUp() {
-        
-        String browser = System.getProperty("browser", "chrome");
+        logger.info("Настройка ChromeOptions для запуска...");
+        ChromeOptions options = new ChromeOptions();
 
-        
-        boolean isHeadless = System.getProperty("GITHUB_ACTIONS") != null;
 
-        logger.info("Запуск тестов в браузере: " + browser + (isHeadless ? " [Headless Mode]" : ""));
+        options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--window-size=1920,1080");
 
-        if (browser.equalsIgnoreCase("chrome")) {
-            ChromeOptions options = new ChromeOptions();
-            if (isHeadless) options.addArguments("--headless", "--disable-gpu", "--window-size=1920,1080");
-            app.init(browser, options);
 
-        } else if (browser.equalsIgnoreCase("firefox")) {
-            FirefoxOptions options = new FirefoxOptions();
-            if (isHeadless) options.addArguments("--headless");
-            app.init(browser, options);
+        System.out.println("Аргументы Chrome: " + options.asMap().get("goog:chromeOptions"));
 
-        } else if (browser.equalsIgnoreCase("edge")) {
-            EdgeOptions options = new EdgeOptions();
-            if (isHeadless) options.addArguments("--headless");
-            app.init(browser, options);
-        }
+        app.init("chrome", options);
     }
 
     @AfterMethod(alwaysRun = true)
