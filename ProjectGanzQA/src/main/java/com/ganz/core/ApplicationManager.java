972c9851ef441060aa8e1471e0e73fa72ccd2059
Oleh Hanziienko <ohanziienko@hotmail.com>
2026-02-19 19:22:51 +0100

    refactor: fix project structure to src/test/java and rename package to ui

--- ProjectGanzQA/src/main/java/com/ganz/core/ApplicationManager.java
+++ ProjectGanzQA/src/main/java/com/ganz/core/ApplicationManager.java
@@ -1,50 +1,33 @@
 package com.ganz.core;
-import com.ganz.fw.ItemHelper;
-import com.ganz.fw.UserHelper;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.edge.EdgeOptions;
 import java.time.Duration;
 
 public class ApplicationManager {
     public WebDriver driver;
-    UserHelper userHelper;
-    ItemHelper itemHelper;
 
-    public void init(String browser, Object options) {
-        if (browser.equalsIgnoreCase("chrome")) {
-            driver = new ChromeDriver((ChromeOptions) options);
-        } else if (browser.equalsIgnoreCase("firefox")) {
-            driver = new FirefoxDriver((FirefoxOptions) options);
-        } else if (browser.equalsIgnoreCase("edge")) {
-            driver = new EdgeDriver((EdgeOptions) options);
-        }
+    public void init() {
+        ChromeOptions options = new ChromeOptions();
 
-        if (driver != null) {
-            driver.get("https://demowebshop.tricentis.com/");
-            driver.manage().window().maximize();
-            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        // Настройки для работы внутри Docker/Linux
+        options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--window-size=1920,1080");
 
-            userHelper = new UserHelper(driver);
-            itemHelper = new ItemHelper(driver);
-        }
+        driver = new ChromeDriver(options);
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://www.demoblaze.com/");
     }
 
     public void stop() {
         if (driver != null) {
             driver.quit();
         }
-    }
-
-    public UserHelper getUser() {
-        return userHelper;
-    }
-
-    public ItemHelper getItem() {
-        return itemHelper;
     }
 }
